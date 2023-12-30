610cac470f5945a35b394f264da8c9db7937fe00
Kristoffer <kristofferhumle@gmail.com>
2023-12-30 23:51:57 +0400

    Making interval

--- water/main.py
+++ water/main.py
@@ -1,15 +1,18 @@
 import rclpy
 from . import pump
+from . import utils
 from rclpy.node import Node
 from std_msgs.msg import String
 
 DAY = 'day'
 NIGHT = 'night'
 
-class Pump(Node):
+MIN_INTERVAL = 10
+
+class Water(Node):
 
     def __init__(self):
-        super().__init__('sun')
+        super().__init__('water')
         self.pump = pump.Pump()
         self.subscription = self.create_subscription(
             String,
@@ -22,16 +25,24 @@ class Pump(Node):
 
         self.get_logger().info('Getting: "%s"' % msg.data)
         
-        if msg.data == DAY:
-            self.pump.flow()
-        
-        elif msg.data == NIGHT:
+        if msg.data == NIGHT or not self.is_active_periode():
             self.pump.stop()
+            self.get_logger().info('Stopping pump')
 
+        elif msg.data == DAY:
+            self.pump.flow()
+            self.get_logger().info('Staring pump')
+        
+    def is_active_periode(self):
+        nanoseconds = self.get_clock().now().nanoseconds
+        minute_of_day = utils.nanoseconds_to_minutes(nanoseconds)
+
+        return minute_of_day < MIN_INTERVAL
+  
 def main(args=None):
     rclpy.init(args=args)
 
-    pump_subscriber = Pump()
+    pump_subscriber = Water()
 
     rclpy.spin(pump_subscriber)
 
