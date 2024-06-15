a71522d38588baf60752a631332d437d9bd1e61d
Kristoffer <kristofferhumle@gmail.com>
2024-06-15 12:10:24 +0400

    Support water interval length

--- water/main.py
+++ water/main.py
@@ -4,11 +4,14 @@ from . import pump
 from . import utils
 from rclpy.node import Node
 from std_msgs.msg import String
+import time
+from datetime import datetime
 
 DAY = 'day'
 NIGHT = 'night'
 PIN = int(os.getenv('PIN',15))
 MIN_INTERVAL = int(os.getenv('MIN_INTERVAL',10))
+WATER_MIN_MINUTE_DIFF = int(os.getenv('WATER_MIN_MINUTE_DIFF', 3))
 
 class Water(Node):
 
@@ -36,9 +39,22 @@ class Water(Node):
             self.get_logger().info('Staring pump')
         
     def is_active_periode(self):
+
+        try:
+            last_run_difference = datetime.now() - self.last_run
+            minutes = last_run_difference.total_seconds() / 60  
+            self.get_logger().info('Minutes since last run: "%d"' % minutes)
+            if minutes < WATER_MIN_MINUTE_DIFF:
+                self.get_logger().info('No water - too close to last run')
+                return False
+        
+        except AttributeError:
+            self.get_logger().info('Inital run. Setting last_run')
+            self.last_run = datetime.now()
+        
         nanoseconds = self.get_clock().now().nanoseconds
         minute_of_day = utils.nanoseconds_to_minutes(nanoseconds)
-
+        
         return minute_of_day < MIN_INTERVAL
   
 def main(args=None):
